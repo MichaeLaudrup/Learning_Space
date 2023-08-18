3813e2e1293dc65860bd217c1641e0871f320473
MichaeLaudrup <maikelaudrupluisgonzalez@gmail.com>
2023-08-18 19:30:43 +0200

    added react repo

---
+++ React_zone/jsx/src/components/user-file.component.jsx
@@ -0,0 +1,9 @@
+export function UserPost(props){
+    return <div class="post-component">
+        <img src="" alt="" />
+        <div className="user-data">
+            <h4 className="user-data__name"> Michael</h4>
+            <div className="user-data__post">Opino que react mola</div>
+        </div>
+    </div>
+}
